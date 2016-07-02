2388d42844a187486eed96b4ef00df009bc87b2f
DD <diego@diegodiazweb.com>
2016-07-02 16:34:59 -0300

    Finish homepage

--- index.php
+++ index.php
@@ -3,39 +3,55 @@
 <?php $header_static = true; ?>
 <?php require_once('includes/header.php'); ?>
 
+<?php //Contenido va acá ?>
+
 <!-- Slider -->
-<div class="swiper-container home-slider text-center">
+<section class="swiper-container home-slider">
     <!-- Required wrapper -->
-    <div class="swiper-wrapper">
+    <div class="swiper-wrapper hidden-xs">
         <!-- Slides -->
         <div class="swiper-slide slide-cabana">
-          <h2 class="h2">La selección de una cabaña<br>se fundamenta en tener información precisa.</h2>
+            <h2 class="h2">La selección de una cabaña<br>se fundamenta en tener información precisa.</h2>
+            <a class="btn btn-primary h5" href="cabana">LA CABAÑA</a>
         </div>
-        <div class="swiper-slide slide-ventas">Slide 2</div>
-        <div class="swiper-slide slide-contacto">Slide 3</div>
-        <div class="swiper-slide slide-remate">Slide 4</div>
+        <div class="swiper-slide slide-ventas">
+            <h2 class="h2">Esto es fertilidad, mucha más retribución<br>con el mismo capital en vientres.</h2>
+            <a class="btn btn-primary h5" href="cabana">¿QUÉ VENDEMOS?</a>
+        </div>
+        <div class="swiper-slide slide-contacto">
+            <h2 class="h2">Estamos agradecidos por tener la oportunidad<br>de buscar la excelencia disfrutando lo que hacemos. </h2>
+            <a class="btn btn-primary h5" href="cabana">CONTACTO</a>
+        </div>
+        <div class="swiper-slide slide-remate">
+            <img class="img-fluid" src="assets/img/home/img-slider-4-remate.jpg" alt="70 Toros Angus de 2 años">
+            <h2 class="h2">Remate de la cabaña<br>en la localidad de Balcarce<br>organización de la firma<br>Fontana y cia.</h2>
+        </div>
     </div>
     
     <!-- Navigation buttons -->
-    <div class="swiper-button-prev swiper-button-white"></div>
-    <div class="swiper-button-next swiper-button-white"></div>
-</div>
-
-
-
-
-
-<?php //Contenido va acá ?>
-<section class="container">
-    <div class="row">
-        <div class="col-md-4 col-xs-12">asdsa</div>
-        <div class="col-md-8 col-xs-12">asdasdasd</div>
-    </div>
-    <div class="row">
-        <div class="col-md-8 col-xs-12">asdasd</div>
-        <div class="col-md-4 col-xs-12">asdasdasdasd</div>
-    </div>
+    <div class="swiper-button-prev swiper-button-white hidden-xs"></div>
+    <div class="swiper-button-next swiper-button-white hidden-xs"></div>
+  
+  
+    <div class="container">
+      <img class="img-responsive visible-xs" src="assets/img/home/img-slider-4-remate.jpg" alt="70 Toros Angus de 2 años">
+      <div class="row">
+          <div class="col-sm-4 col-xs-12">
+              <h3 class="h4">FERTILIDAD</h3>
+              <p>Hemos logrado el 95% de preñez en 65 días de servicio en los últimos años. Trabajamos con una carga de 1.2 vacas por hectárea ganadera, sin aportes de suplementos al sistema.</p>
+          </div>
+          <div class="col-sm-4 col-xs-12">
+              <h3 class="h4">FACILIDAD DE PARTO</h3>
+              <p>Implementando al servicio hiperprecoz, logramos el 83.5% de terneros marcados sobre vientre ingresado a servicio de reposición en el destete 2015.</p>
+          </div>
+          <div class="col-sm-4 col-xs-12">
+              <h3 class="h4">FENOTIPO FUNCIONAL</h3>
+              <p>El 43 % de los machos nacidos son vendidos como reproductores, el resto se capa y se engorda o vende al destete. Asegurando una alta precisa selección.</p>
+          </div>
+      </div>
+  </div>
 </section>
+
 <?php //Y termina acá ?>
 
 <?php require_once('includes/formulario.php'); ?>
