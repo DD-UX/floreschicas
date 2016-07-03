23ef6cdb4290c7e810fce187990905f1aaaea10d
DD <diego@diegodiazweb.com>
2016-07-03 14:32:04 -0300

    Hardfix renombrando ventas como contacto

--- contacto.php
+++ contacto.php
@@ -1,19 +1,113 @@
-<?php $titulo = "Cabaña flores chicas - Bienvenidos!"; ?>
+<?php $titulo = "Contacto - Cabaña flores chicas"; ?>
 <?php $body_classes = "contacto spacer"; ?>
 <?php require_once('includes/header.php'); ?>
 
 <?php //Contenido va acá ?>
 <section class="container">
     <div class="row">
-        <div class="col-md-4 col-xs-12">asdsa</div>
-        <div class="col-md-8 col-xs-12">asdasdasd</div>
-    </div>
-    <div class="row">
-        <div class="col-md-8 col-xs-12">asdasd</div>
-        <div class="col-md-4 col-xs-12">asdasdasdasd</div>
+        <div class="col-md-5 col-xs-12 padding-50-left contacto-info">
+            <h2 class="h4 text-uppercase">CONTACTO</h2>
+			<dl>
+				<dt>Director</dt>
+				<dd>Ingeniero Agronomo Alberto S. Areco</dd>
+				<dt>Movil</dt>
+				<dd>(0223) 155218615 / (02262) 15561061</dd>
+				<dt>Cabaña</dt>
+				<dd>(02261) 442634</dd>
+				<dt>E-Mail</dt>
+				<dd>areco.as@gmail.com</dd>
+				<dt>Dr. Nestor H Reynoso</dt>
+				<dd>Cel: 02262 15567520</dd>
+				<dd>nhreynoso@gmail.com</dd>
+				<dt>Oficina Bs.As.</dt>
+				<dd>Ing. Agrónoma Daniela E. Vazquez Pugliese</dd>
+				<dd>Tel. 011-4802-1790 ó 011-4806-8081</dd>
+			</dl>
+			<p>40 km de Balcarce en dirección a Pieres.<br>5 km después de cruzar la vía, sobre mano derecha.</p>
+        </div>
+        <div class="col-md-7 col-xs-12">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d308225.25996731955!2d-58.57721558421936!3d-38.03063258397145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzjCsDA2JzQ1LjYiUyA1OMKwMzAnMTUuNiJX!5e0!3m2!1sen!2sar!4v1467565939228" width="652" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
     </div>
 </section>
 <?php //Y termina acá ?>
 
 <?php require_once('includes/formulario.php'); ?>
+
+<?php //Contenido va acá ?>
+<section class="container">
+	<div class="row">
+		<div class="panel-group" id="accordion">
+			<div class="panel panel-default">
+
+				<!-- Comida -->
+				<a data-toggle="collapse" data-parent="#accordion" href="#comida" class="collapsed">
+					<div class="panel-heading bg-verde colapsador">
+						<h3 class="h4 text-uppercase">¿QUE LES DAN  DE COMER A LOS TOROS?</h3>
+						<div class="colapsador-button-next"></div>
+					</div>
+				</a>
+				<div id="comida" class="panel-collapse collapse row">
+					<div class="col-sm-6 col-xs-12 padding-50-left">
+						<p>Luego de destetar a los toros a los 4-5 meses de edad, van a un sorgo de pastoreo. Después y hasta la venta están en promociones de raigrass y pasturas en lotes de 70-90 toros. Cuando el pasto no es suficiente, llenamos las tolvas de autoconsumo con cascarilla de soja y pellet de girasol o cártamo.</p>
+					</div>
+					<div class="col-sm-6 col-xs-12 padding-50-right">
+						<p>Este suplemento es fibroso, no necesita acostumbramiento y evitamos el almidón, posible fuente de acidosis subclínica, y responsable de futuros problemas podales. Gracias a esta comida, los toros no sufren al ir al campo del comprador, ya que estos suplementos no cambian la flora ruminal de los toros, no se "desinflan" como generalmente pasa al engordarlos con maíz.</p>
+					</div>
+				</div>
+
+				<!-- Toros Propios -->
+				<a data-toggle="collapse" data-parent="#accordion" href="#toros-propios" class="collapsed">
+					<div class="panel-heading bg-verde colapsador">
+						<h3 class="h4 text-uppercase">¿PORQUE USAN TANTOS TOROS PROPIOS COMO PADRES DE PLANTEL?</h3>
+						<div class="colapsador-button-next"></div>
+					</div>
+				</a>
+				<div id="toros-propios" class="panel-collapse collapse row">
+					<div class="col-sm-6 col-xs-12 padding-50-left">
+						<p>Nosotros seleccionamos por características que habitualmente no están en los catálogos. La fertilidad solo aparece representada por la circunferencia escrotal, que es útil como un dato para el comprador, pero para seleccionar en una cabaña se necesita hilar mas fino, con eso solo no nos alcanza.</p>
+					</div>
+					<div class="col-sm-6 col-xs-12 padding-50-right">
+						<p>Los catálogos tampoco indican qué aplomos tendrán los hijos de los toros que venden y para nosotros, que queremos toros que le duren a nuestros clientes, no podemos darnos el lujo de no saber como saldrán los hijos . Usando nuestra genética tenemos una idea bastante exacta de lo que podemos esperar.</p>
+					</div>
+				</div>
+
+				<!-- Diferencias -->
+				<a data-toggle="collapse" data-parent="#accordion" href="#diferencias" class="collapsed">
+					<div class="panel-heading bg-verde colapsador">
+						<h3 class="h4 text-uppercase">¿QUE CREEN QUE LOS DIFERENCIA DE LOS COMPETIDORES?</h3>
+						<div class="colapsador-button-next"></div>
+					</div>
+				</a>
+				<div id="diferencias" class="panel-collapse collapse row">
+					<div class="col-sm-6 col-xs-12 padding-50-left">
+						<p>Nos parece que el haber certificado normas ISO 9001 - 2008 de gestión de calidad nos enseñó a enfocarnos en las necesidades del cliente, que es dificil para gente como nosotros que solo ha trabajado en el medio agropecuario donde nunca hay que salir a vender, todo se vende solo.</p>
+					</div>
+					<div class="col-sm-6 col-xs-12 padding-50-right">
+						<p>Nos enseñó que cuando se ejecuta una acción que no resulta como se esperaba tenemos una "no conformidad" que se registra y debe ser corregida estableciendo las acciones tendientes a lograrlo. Esto nos parece que nos ha ayudado a ir mejorando de a poco y en forma consistente.</p>
+					</div>
+				</div>
+
+				<!-- Detalles -->
+				<a data-toggle="collapse" data-parent="#accordion" href="#detalles" class="collapsed">
+					<div class="panel-heading bg-verde colapsador">
+						<h3 class="h4 text-uppercase">¿Qué detalles hay que tener en cuenta al comprar un toro de Flores chicas?</h3>
+						<div class="colapsador-button-next"></div>
+					</div>
+				</a>
+				<div id="detalles" class="panel-collapse collapse row">
+					<div class="col-sm-6 col-xs-12 padding-50-left">
+						<p>Nosotros en el sudeste de Buenos Aires usamos el 3% de toros en los rodeos tanto los de 2 años de edad como los de 15 meses de edad  al inicio del servicio. Tratamos en la medida de lo posible de usar lotes de toros de la misma edad. Anotamos los toros que van a cada rodeo. No modificamos el lote de toros en ningún momento y chequeamos cualquier modificación al finalizar el servicio, esto es importante para controlar enfermedades venéreas, evitando la posible transmisión a otro rodeo. </p>
+					</div>
+					<div class="col-sm-6 col-xs-12 padding-50-right">
+						<p>Recomendamos revisar los toros a partir de los 20 días de finalizado el servicio, esto ayuda a detectar tempranamente cualquier problema y prever la cantidad de toros de reemplazo.</p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+    </div>
+</section>
+
+<?php //Y termina acá ?>
 <?php require_once('includes/footer.php'); ?>
