dca793c80e1a193810fbaaeeb7ae3b4ccf22458e
veronica <verosoto1234@gmail.com>
2016-07-03 14:48:39 -0300

    Página de ventas

--- ventas.php
+++ ventas.php
@@ -3,14 +3,68 @@
 <?php require_once('includes/header.php'); ?>
 
 <?php //Contenido va acá ?>
-<section class="container">
+<section class="container box-venta">
     <div class="row">
-        <div class="col-md-4 col-xs-12">asdsa</div>
-        <div class="col-md-8 col-xs-12">asdasdasd</div>
+        <div class="col-md-4 col-xs-12">
+            <img src="assets/img/ventas/img-venta-01.jpg" class="img-responsive" alt="Ventas">
+        </div>
+        <div class="col-md-8 col-xs-12">
+            <div class="bg-verde box-venta-primero box-venta-botton">
+                <h3 class="h5 text-uppercase">Remate anual en Balcarce - 14 de septiembre con Fontana y Cia</h3>
+                <ul class="list-bullet-inside">
+                    <li>70 toros de 2 años - 50% apto para servicio de vaquillonas de 15 meses.</li>
+                    <li>10 toros de 15 meses aptos para servicio de vaquillonas de 15 meses.</li>
+                    <li>150 vaquillonas parición otoño 2017.</li>
+                    <li>Servicio de IATF a parir en el lapso de 1 mes.</li>
+                </ul>
+            </div>
+            <div class="bg-verde box-venta-primero">
+                <h3 class="h5 text-uppercase">Venta particular de toros</h3>
+                <ul class="list-bullet-inside">
+                    <li>Toros de 18 a 24 meses de edad, en venta desde marzo a octubre.</li>
+                    <li>Toros de 13 a 17 meses de edad, en venta desde septiembre a enero.</li>
+                </ul>
+                <h3 class="h5 text-uppercase">Se clasifican según las necesidades del cliente</h3>
+                <ul class="list-bullet-inside">
+                    <li>Toros aptos para servicio de vaquillonas de 15 meses y en cualquier categoría.</li>
+                    <li>Toros aptos para usar en vaquillonas de 18 a 27 meses y en cualquier categoría.</li>
+                    <li>Toros aptos para utilizar solo en vacas.</li>
+                </ul>
+            </div>
+        </div>
     </div>
     <div class="row">
-        <div class="col-md-8 col-xs-12">asdasd</div>
-        <div class="col-md-4 col-xs-12">asdasdasdasd</div>
+        <div class="col-md-4 col-xs-12">
+            <img src="assets/img/ventas/img-venta-02.jpg" class="img-responsive" alt="Ventas">
+        </div>
+        <div class="col-md-8 col-xs-12">
+            <div class="bg-verde box-venta-segundo">
+                <h3 class="h5 text-uppercase">Mecanismo de venta particular de toros</h3>
+                <ol class="list-bullet-inside">
+                    <li>El interesado se comunica  1 o 2 días antes de la revisación en el campo<br>
+especificando que categoría/s y cantidad de toros necesita.</li>
+                    <li>El interesado concurre al campo a elegir los toros, la cantidad de toros presentados para su elección es normalmente 3 o más veces superior a la cantidad requerida, posibilitándole elegir con comodidad.</li>
+                    <li>Si el interesado quiere revisar los toros con su asesor veterinario contará con la<br>
+asistencia de nuestro personal e instalaciones.</li>
+                    <li>Es importante que concurran con los datos necesarios para organizar la carga<br>
+y facturación.</li>
+                    <li>El interesado contará con la información y asistencia necesaria para elegir los
+toros que más le convengan.</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-xs-12">
+            <img src="assets/img/ventas/img-venta-03.jpg" class="img-responsive" alt="Ventas">
+        </div>
+        <div class="col-md-8 col-xs-12">
+            <div class="bg-verde box-venta-tercero">
+                <h3 class="h5 text-uppercase">Venta particular de hembras</h3>
+                <p>Se ofrecen a la venta a partir de Agosto.
+Vaquillonas de 24 meses de edad. Parición otoño 2017. Servidas con IATF y repaso con toros de parto fácil. Se venden con información de: Fecha de nacimiento, N° del padre, N° de la madre, peso al nacer y fecha aproximada de parto.</p>
+            </div>
+        </div>
     </div>
 </section>
 <?php //Y termina acá ?>
